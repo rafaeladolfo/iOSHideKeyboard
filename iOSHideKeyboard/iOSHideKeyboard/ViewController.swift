ce52e16835b5ef3929b08222859a255f6bd433b8
rfa1989 <rafael@innovetech.com.br>
2018-01-30 23:36:08 -0200

    Hide keyboard when touch
    When touching outside the textfield, hides the keyboard

--- iOSHideKeyboard/iOSHideKeyboard/ViewController.swift
+++ iOSHideKeyboard/iOSHideKeyboard/ViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var testText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.testText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
